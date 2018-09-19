14d70f1ba4fa05070f9b280ef928c4946e13b9f5
sameer <sameersaini40@gmail.com>
2018-09-19 14:29:25 -0600

    added solution for BonAppetit

---
+++ Implementation/BonAppetit.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the bonAppetit function below.
+def bonAppetit(bill, k, b):
+    bill.pop(k)
+    actualBill = sum(bill)//2
+    if actualBill == b: print("Bon Appetit")
+    else: print(b - actualBill)
+
+if __name__ == '__main__':
+    nk = input().rstrip().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
