8fc0d36c43f9b92af943c8b36d54693dab990140
sameer <sameersaini40@gmail.com>
2018-12-02 23:59:25 -0700

    added solution for removeDuplicates

---
+++ Array/removeDuplicates.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d = {}
+        i = 0
+
+        while i < len(nums):
+            if nums[i] in d:
+                nums.pop(i)
+            else:
+                d[nums[i]] = 1
+                i += 1
+
+        return len(nums)
