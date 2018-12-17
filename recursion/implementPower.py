d10ca217059fb88a91b3e714a79c292eb1cfc1d4
sameer <sameersaini40@gmail.com>
2018-12-16 19:00:40 -0700

    Added solution for implementPower

---
+++ recursion/implementPower.py
@@ -0,0 +1,20 @@
+class Solution:
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0:
+            return 1
+
+        temp = self.myPow(x, int(n/2))
+
+        if n % 2 ==0:
+            return temp * temp
+        else:
+            if n > 0:
+                return x * temp * temp
+            else:
+                return (temp * temp)/x
+
