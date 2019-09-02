a5436ab598deac9a76b26a3371a9c5d351babcca
sameer <sameersaini40@gmail.com>
2019-09-01 22:28:22 -0600

    adds solution for leetcode searchBST

---
+++ Trees/searchBST.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if not root or root.val == val:
+            return root
+
+        return self.searchBST(root.left, val) if root.val > val else self.searchBST(root.right, val)
