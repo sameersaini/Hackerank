1ff6eea4d355df1edeed85955c535b56150fffb5
sameer <sameersaini40@gmail.com>
2019-09-05 09:29:21 -0600

    updates solution for moveZeros

--- Array/moveZeros.py
+++ Array/moveZeros.py
@@ -4,23 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        left = 0
+        start = 0
 
-        while left < len(nums):
-            while left < len(nums) and nums[left] != 0:
-                left += 1
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[start] = nums[i]
+                start += 1
 
-            if left == len(nums):
-                break
-
-            temp = left + 1
-
-            while temp < len(nums) and nums[temp] == 0:
-                temp += 1
-
-            if temp == len(nums):
-                break
-
-            nums[left], nums[temp] = nums[temp], nums[left]
-
-        return left
+        for i in range(start, len(nums)):
+            nums[i] = 0
